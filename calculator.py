b43d8fa93bb9c7d02a90380fbb8b19b8fccf06b2
Michael Daley <69087473+MDaley08@users.noreply.github.com>
2026-05-27 23:33:03 -0400

    added a validate function to calculator.py to check that inputs are valid numbers and defined add, sub, div and mult functions

---
+++ calculator.py
@@ -0,0 +1,22 @@
+def validate_num(a):
+    """
+    Check if input is a valid Number
+
+    Args:
+        a: The input value of any type to check.
+    Returns:
+        True if input is an int or float, False otherwise
+    """
+    return isinstance(a,(int,float,complex)) and  not isinstance(a,bool)
+
+def addition(a,b):
+    pass
+
+def subtraction(a,b):
+    pass
+
+def multiplication():
+    pass
+
+def division():
+    pass
