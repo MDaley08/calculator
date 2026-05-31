b62159d5280a00402dd2a46bd56e78ac333a96f9
michel <69087473+MDaley08@users.noreply.github.com>
2026-05-31 00:15:10 -0400

    updated addition, subtraction, multiplication and division functions to perform the matching arithmetic operations

--- calculator.py
+++ calculator.py
@@ -10,13 +10,62 @@ def validate_num(a):
     return isinstance(a,(int,float,complex)) and  not isinstance(a,bool)
 
 def addition(a,b):
-    pass
+    """
+    adds two numbers
+
+    Args:
+        a: first number to add
+        b: second number to add
+    Returns:
+        the sum of a and b
+    """
+    if(validate_num(a) and validate_num(b)):
+        return a + b
+    else:   
+        return "invalid input" #place holder, implement proper error later
 
 def subtraction(a,b):
-    pass
+    """
+    subtracts b from a
 
-def multiplication():
-    pass
+    Args:
+        a: the minuend(number being subtracted from)
+        b: subtrahend(number being subtracted)
+    Returns:
+        the difference of b from a
+    """
+    if(validate_num(a) and validate_num(b)):
+        return a - b
+    else:   
+        return "invalid input" #place holder, implement proper error later
 
-def division():
-    pass
+def multiplication(a,b):
+    """
+    multiplies a and b
+
+    Args:
+        a: first factor to be multiplied
+        b: second factor to be multiplied
+    Returns:
+        the product of a and b
+    """
+    if(validate_num(a) and validate_num(b)):
+        return a * b
+    else:   
+        return "invalid input" #place holder, implement proper error later
+
+def division(a,b):
+    """
+    divided a by b
+
+    Args:
+        a: the dividend
+        b: the divisor
+    Returns:
+        returns the quotient of a and b
+    """
+    
+    if(validate_num(a) and validate_num(b)):
+        return a / b
+    else:   
+        return "invalid input" #place holder, implement proper error later
