3a22d29306538247d867324367e953aff23efbc2
michel <69087473+MDaley08@users.noreply.github.com>
2026-06-01 17:19:45 -0400

    added test functionality for positive and negative addition

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,11 +3,15 @@ from app.operations import addition, subtraction, multiplication, division
 
 def test_addition_positive():
     """test positive cases for additon"""
-    pass
+    assert addition(1,1) == pytest.approx(2.0)
+    assert addition(-1,2) == pytest.approx(1.0)
+    assert addition(3,0) == pytest.approx(3.0)
 
 def test_addition_negative():
     """test negative cases for additon"""
-    pass
+    assert addition(-3,-1) == pytest.approx(-4.0)
+    assert addition(-3,1) == pytest.approx(-2.0)
+    assert addition(-3,0) == pytest.approx(-3.0)
 
 def test_subtraction_postitive():
     """test positive cases for subtraction"""
