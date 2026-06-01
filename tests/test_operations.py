263e412799e9bd1a66a5715342487c3427b66948
michel <69087473+MDaley08@users.noreply.github.com>
2026-06-01 18:56:09 -0400

    added functionality and error handling for positive, negative and division by 0

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -20,23 +20,33 @@ def test_subtraction_postitive():
     assert subtraction(2.0,0.0) == pytest.approx(2.0)
 
 def test_subtraction_negative():
-    """test negative cases for multiplication"""
+    """test negative cases for subtraction"""
     assert subtraction(1.0,3.0) == pytest.approx(-2.0)
     assert subtraction(-4.0,-3.0) == pytest.approx(-1.0)
     assert subtraction(-1.0,0.0) == pytest.approx(-1.0)
 
+def test_multiplication_positive():
+    """test positive cases for multiplication""" 
+    assert multiplication(1.0, 3.0) == pytest.approx(3.0)
+    assert multiplication(-2.0, -3.0) == pytest.approx(6.0)
+    assert multiplication(3.0, 0.0) == pytest.approx(0.0)
+
 def test_multiplication_negative():
     """test negative cases for multiplication"""
-    pass
+    assert multiplication(-1.0,3.0) == pytest.approx(-3.0)
+    assert multiplication(3.0,-8.0) == pytest.approx(-24.0)
 
 def test_division_positive():
     """test positive cases for division"""
-    pass
+    assert division(3.0, 1.0) == pytest.approx(3.0)
+    assert division(-6.0, -3.0) == pytest.approx(2.0)
 
 def test_division_negative():
     """test negative cases for division"""
-    pass
+    assert division(-3.0, 1.0) == pytest.approx(-3.0)
+    assert division(6.0, -3.0) == pytest.approx(-2.0)
 
 def test_division_zero():
-    """test """
-    pass
+    """test ValueError for division by zero"""
+    with pytest.raises(ValueError, match="Cannot divide by 0"):
+        division(1.0,0)
