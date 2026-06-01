00577287bc2342c1ec78e46f0155c0f0d14b1eb9
michel <69087473+MDaley08@users.noreply.github.com>
2026-06-01 17:08:48 -0400

    defined test cases for operation functions

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,1 +1,34 @@
 import pytest 
+from app.operations import addition, subtraction, multiplication, division
+
+def test_addition_positive():
+    """test positive cases for additon"""
+    pass
+
+def test_addition_negative():
+    """test negative cases for additon"""
+    pass
+
+def test_subtraction_postitive():
+    """test positive cases for subtraction"""
+    pass
+
+def test_multiplication_positive():
+    """test positive cases for multiplication"""
+    pass
+
+def test_multiplication_negative():
+    """test negative cases for multiplication"""
+    pass
+
+def test_division_positive():
+    """test positive cases for division"""
+    pass
+
+def test_division_negative():
+    """test negative cases for division"""
+    pass
+
+def test_division_zero():
+    """test """
+    pass
