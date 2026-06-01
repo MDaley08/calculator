83dc3e79dfd17be161a0b63be838121f36d3685b
michel <69087473+MDaley08@users.noreply.github.com>
2026-06-01 18:22:41 -0400

    added test functionality for positive and negative subtraction

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -3,23 +3,27 @@ from app.operations import addition, subtraction, multiplication, division
 
 def test_addition_positive():
     """test positive cases for additon"""
-    assert addition(1,1) == pytest.approx(2.0)
-    assert addition(-1,2) == pytest.approx(1.0)
-    assert addition(3,0) == pytest.approx(3.0)
+    assert addition(1.0,1.0) == pytest.approx(2.0)
+    assert addition(-1.0,2.0) == pytest.approx(1.0)
+    assert addition(3.0,0.0) == pytest.approx(3.0)
 
 def test_addition_negative():
     """test negative cases for additon"""
-    assert addition(-3,-1) == pytest.approx(-4.0)
-    assert addition(-3,1) == pytest.approx(-2.0)
-    assert addition(-3,0) == pytest.approx(-3.0)
+    assert addition(-3.0,-1.0) == pytest.approx(-4.0)
+    assert addition(-3.0,1.0) == pytest.approx(-2.0)
+    assert addition(-3.0,0.0) == pytest.approx(-3.0)
 
 def test_subtraction_postitive():
     """test positive cases for subtraction"""
-    pass
+    assert subtraction(3.0,1.0) == pytest.approx(2.0)
+    assert subtraction(4.0,-3.0) == pytest.approx(7.0)
+    assert subtraction(2.0,0.0) == pytest.approx(2.0)
 
-def test_multiplication_positive():
-    """test positive cases for multiplication"""
-    pass
+def test_subtraction_negative():
+    """test negative cases for multiplication"""
+    assert subtraction(1.0,3.0) == pytest.approx(-2.0)
+    assert subtraction(-4.0,-3.0) == pytest.approx(-1.0)
+    assert subtraction(-1.0,0.0) == pytest.approx(-1.0)
 
 def test_multiplication_negative():
     """test negative cases for multiplication"""
