dcd65933ff2cafa5c896ce09ae8c7ce29a1772d4
michel <69087473+MDaley08@users.noreply.github.com>
2026-06-02 17:47:48 -0400

    added test cases to account for invalid input in our operations

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,6 +1,14 @@
 import pytest 
 from app.operations import addition, subtraction, multiplication, division
 
+
+def test_addition_invalid_input():
+    "test invalid values for additon"
+    assert addition('a', 1)
+    assert addition(1, 'a')
+    assert addition('a', 'b')
+
+
 def test_addition_positive():
     """test positive cases for additon"""
     assert addition(1.0,1.0) == pytest.approx(2.0)
@@ -12,6 +20,13 @@ def test_addition_negative():
     assert addition(-3.0,-1.0) == pytest.approx(-4.0)
     assert addition(-3.0,1.0) == pytest.approx(-2.0)
     assert addition(-3.0,0.0) == pytest.approx(-3.0)
+
+
+def test_subtraction_invalid_input():
+    "test invalid values for multiplication"
+    assert subtraction('a', 1)
+    assert subtraction(1, 'a')
+    assert subtraction('a', 'b')
 
 def test_subtraction_postitive():
     """test positive cases for subtraction"""
@@ -25,6 +40,12 @@ def test_subtraction_negative():
     assert subtraction(-4.0,-3.0) == pytest.approx(-1.0)
     assert subtraction(-1.0,0.0) == pytest.approx(-1.0)
 
+def test_multiplication_invalid_input():
+    "test invalid values for multiplicaiton"
+    assert multiplication('a', 1)
+    assert multiplication(1, 'a')
+    assert multiplication('a', 'b')
+
 def test_multiplication_positive():
     """test positive cases for multiplication""" 
     assert multiplication(1.0, 3.0) == pytest.approx(3.0)
@@ -35,6 +56,12 @@ def test_multiplication_negative():
     """test negative cases for multiplication"""
     assert multiplication(-1.0,3.0) == pytest.approx(-3.0)
     assert multiplication(3.0,-8.0) == pytest.approx(-24.0)
+
+def test_division_invalid_input():
+    "test invalid values for additon"
+    assert division('a', 1)
+    assert division(1, 'a')
+    assert division('a', 'b')
 
 def test_division_positive():
     """test positive cases for division"""
